859e54be384ffc0e55b580397848e749b4cb629a
User <splimis@gmail.com>
2017-09-28 09:38:24 -0400

    initial

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'a1nFATwEALTra6oL24uqJyJ9J',
+  consumer_secret: '56OLmnJ1157CHg4kHzZgSswugcxCVrsHu8jrQbjXangFw6Gw1A',
+  access_token_key: '912823445695721472-dep1VvwfAk1x654E7y2Rumiwuc3Jl8Z',
+  access_token_secret: 'ArkHApaho1kAkYqEiwzSFOSeo8Ds3rSbd6CxskJERLFna',
+}
+
+module.exports = twitterKeys;
